7d5cb9155b29beb4abb575d8ba41df31cc311a03
MuhittinBasarErsoy <basarersoy@gmail.com>
2023-01-08 14:36:12 +0300

    Add UseInMemoryDatabase Add Customer Class

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,7 +1,10 @@
 ﻿//Minimal API -- Giriş ve Swagger Kullanımı
 
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<CustomerDb>(options => options.UseInMemoryDatabase("CustomerDb"));
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
@@ -13,3 +16,27 @@ app.UseSwaggerUI(c =>
 
 app.MapGet("/",()=>"Hello World");
 app.Run();
+
+
+
+class Customer
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+
+}
+
+class CustomerDb : DbContext
+{
+	public CustomerDb(DbContextOptions options) : base(options)
+	{
+		
+	}
+
+	public DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.UseInMemoryDatabase("CustomerDb");
+    }
+}
